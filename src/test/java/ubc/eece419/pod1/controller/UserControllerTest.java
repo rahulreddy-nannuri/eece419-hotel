72dedb758a8dc00615a5c67dfea5f38fb156931f
ngentleman <ngentleman@649b2da4-a7a6-11de-b2a4-832221a088ed>
2009-10-30 19:13:10 +0000

    Create-user test needed to set authentication.

--- src/test/java/ubc/eece419/pod1/controller/UserControllerTest.java
+++ src/test/java/ubc/eece419/pod1/controller/UserControllerTest.java
@@ -10,52 +10,51 @@ import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.GrantedAuthorityImpl;
+import org.springframework.security.context.SecurityContextHolder;
+import org.springframework.security.providers.anonymous.AnonymousAuthenticationToken;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.web.servlet.ModelAndView;
 
 import ubc.eece419.pod1.entity.User;
+import ubc.eece419.pod1.security.SecurityUtils;
 import ubc.eece419.pod1.dao.UserRepository;
 
-public class UserControllerTest{
+public class UserControllerTest {
 
 	Mockery context  = new Mockery();
-	
-	private UserRepository userRepository;
-	private UserController userController;
 
-	User getEntity() {
-		// TODO Auto-generated method stub
-		return new User();
-	}
+	UserRepository userRepository;
+	UserController userController;
 
 	@Before
 	public void setUp() {
-		// TODO Auto-generated method stub
 		userRepository = context.mock(UserRepository.class);
 		userController = new UserController();
 		userController.userRepository = userRepository;
 	}
-	
+
 	@Test
 	public void testSaveEmptyUser(){
 		final User user = new User();
-		
+
 		context.checking(new Expectations(){{
 			oneOf(userRepository).findAll();
 			will(returnValue(Collections.emptyList()));
 			}
 		});
-		
+
 		BeanPropertyBindingResult errors = new BeanPropertyBindingResult(user, "user");
 
 		ModelAndView mav = userController.save(user, errors, null);
-		
+
 		//how do i know how many errors do i have
 		//2 error means two methods fail? getPassword and getAuthority?
 		assertEquals(2, errors.getErrorCount());
-		
+
 		User model = (User) mav.getModel().get("user");
-		
+
 		assertEquals(null, model.getAddress());
 		assertEquals(null, model.getUsername());
 		assertEquals(null, model.getEmail());
@@ -64,62 +63,67 @@ public class UserControllerTest{
 		assertEquals("user", model.getEntityName());
 		assertEquals(0, (int)model.getId());
 		assertEquals("ROLE_USER", model.getRoles());
-		
+
 		assertEquals(1, mav.getModel().size());
 	}
-	
+
+	public static void setLoggedInUser(User user) {
+		if (user == null) {
+			GrantedAuthority anon = new GrantedAuthorityImpl("ROLE_ANONYMOUS");
+			AnonymousAuthenticationToken auth = new AnonymousAuthenticationToken("key", "Anonymous", new GrantedAuthority[] { anon });
+			SecurityContextHolder.getContext().setAuthentication(auth);
+		} else {
+			SecurityUtils.login(user);
+		}
+	}
+
 	@Test
-	public void testUserWithNoMissingValus(){
+	public void testUserWithNoMissingValues(){
 		final User user = new User("tempName", "tempPwd", "ROLE_ADMIN");
-		
-		context.checking(new Expectations(){
-			{
-				oneOf(userRepository).findAll();
-				will(returnValue(Collections.emptyList()));
-				
-				//oneOf(userRepository).save(user);
-				//will(returnValue(user));
-			}
-		});
-		
+
+		setLoggedInUser(null);
+
+		context.checking(new Expectations() {{
+			exactly(2).of(userRepository).findAll();
+			will(returnValue(Collections.emptyList()));
+
+			one(userRepository).save(user);
+			will(returnValue(user));
+		}});
+
 		BeanPropertyBindingResult errors = new BeanPropertyBindingResult(user, "user");
 
 		ModelAndView mav = userController.save(user, errors, null);
-		
+
 		assertEquals(0, errors.getErrorCount());
-		
-		User model = (User) mav.getModel().get("user");
-		
-		assertEquals("tempName", model.getUsername());
-		assertEquals("tempPwd", model.getPassword());
-		assertEquals("ROLE_ADMIN", model.getRoles());
+		assertEquals("redirect:/user/list", mav.getViewName());
 	}
-	
+
 	@Test
 	public void testEditUser(){
 		final User user = new User();
-		
+
 		context.checking(new Expectations(){
 			{
 				oneOf(userRepository).findById(1L);
 				will(returnValue(user));
-				
+
 				oneOf(userRepository).findAll();
 			}
 		});
-	
+
 		ModelAndView mav = userController.edit(null);
-		
+
 		assertEquals("/user/edit", mav.getViewName());
 	}
-	
-	
-	
+
+
+
 	@SuppressWarnings("unchecked")
 	@Test
 	public void testEmptyList(){
 		List<User> entities = new ArrayList<User>();
-		
+
 		context.checking(new Expectations(){
 			{
 				oneOf(userRepository).findAll();
@@ -127,10 +131,10 @@ public class UserControllerTest{
 		});
 
 		ModelAndView mav = userController.list();
-		List<User> model = (List<User>) mav.getModel().get(getEntity().getEntityName() + "s");
+		List<User> model = (List<User>) mav.getModel().get(new User().getEntityName() + "s");
 		assertEquals(entities.size(), model.size());
 		assertEquals(model.size(), 0);
 	}
-	
-	
+
+
 }
